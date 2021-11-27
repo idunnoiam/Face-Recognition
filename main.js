c9bd57bc33b582f935d85c4613538afdd36aed41
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2021-11-26 22:02:49 -0900

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot() {
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/848frLbO-/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/UCKWkXtr5/model.json', modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded");
